f1f8991e6c6196029f3611b58a4e814396f16182
TheFRedFox <thefredfox@googlemail.com>
2016-03-14 14:51:18 +0100

    adds IpBehavior class: with this behavior, entities will automatically be filled with the client ip of the request before being saved

---
+++ src/Model/Behavior/IpBehavior.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace IpBehavior\Model\Behavior;
+
+use Cake\Datasource\EntityInterface;
+use Cake\Event\Event;
+use Cake\Network\Request;
+use Cake\ORM\Behavior;
+use UnexpectedValueException;
+
+class IpBehavior extends Behavior
+{
+
+    /** @var Request $_request */
+    public static $_request;
+
+    protected $_defaultConfig = [
+        'implementedFinders' => [],
+        'implementedMethods' => [],
+        'fields' => ['ip' => 'new'],
+    ];
+
+    public function beforeSave(Event $event, EntityInterface $entity)
+    {
+        if ($entity === null) {
+            return true;
+        }
+
+        $isNew = $entity->isNew();
+
+        $fields = $this->config('fields');
+
+        $ip = self::$_request->clientIp();
+
+        foreach ($fields as $field => $when) {
+            $when = strtolower($when);
+            if (!in_array($when, ['always', 'new'])) {
+                throw new UnexpectedValueException(
+                    sprintf('"When" should be one of "always", "new". The passed value "%s" is invalid', $when)
+                );
+            }
+
+            switch ($when) {
+                case 'always': {
+                    $entity->set($field, $ip);
+                    continue;
+                }
+                    break;
+                case 'new': {
+                    if ($isNew) {
+                        $entity->set($field, $ip);
+                        continue;
+                    }
+                }
+                    break;
+            }
+
+        }
+
+        return true;
+    }
+}
